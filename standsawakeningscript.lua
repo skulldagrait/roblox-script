737ffc66cbd8a547c440771e72185d7158c468a2
skulldagrait <laithispro1@gmail.com>
2025-07-23 07:51:45 -0700

    Update standsawakeningscript.lua

--- standsawakeningscript.lua
+++ standsawakeningscript.lua
@@ -1,3 +1,5 @@
+-- Full working script with Teleports, OP, Movement, Boss, Visual, and Credits tabs
+
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Stands Awakening Hub - by skulldagrait",
@@ -20,13 +22,24 @@ local RunService = game:GetService("RunService")
 
 -- Teleport Tab
 local TeleportTab = Window:CreateTab("Teleport", 4483362458)
--- [Insert your teleport buttons here...]
+TeleportTab:CreateButton({
+    Name = "Main Map",
+    Callback = function()
+        LocalPlayer.Character:MoveTo(Vector3.new(0, 10, 0))
+    end
+})
+TeleportTab:CreateButton({
+    Name = "Boss Spawn",
+    Callback = function()
+        LocalPlayer.Character:MoveTo(Vector3.new(200, 10, 0))
+    end
+})
 
--- OP Tab (with AutoFarm)
+-- OP Tab (AutoFarm)
 local OP = Window:CreateTab("OP", 4483362458)
 getgenv().AutoFarm = false
 OP:CreateToggle({
-    Name = "AutoFarm All Items",
+    Name = "AutoFarm Items",
     CurrentValue = false,
     Callback = function(val)
         getgenv().AutoFarm = val
@@ -34,18 +47,39 @@ OP:CreateToggle({
             task.wait(0.5)
             for _,item in pairs(Workspace:GetChildren()) do
                 if item:IsA("Tool") and item:FindFirstChild("Handle") then
-                    HumanoidRootPart.CFrame = item.Handle.CFrame
+                    LocalPlayer.Character.HumanoidRootPart.CFrame = item.Handle.CFrame
                 end
             end
         end
     end,
 })
 
--- Boss Tab (unchanged)
--- [Insert your Boss tab here...]
+-- Boss Tab
+local BossTab = Window:CreateTab("Boss", 4483362458)
+BossTab:CreateButton({
+    Name = "Kill Nearest Boss",
+    Callback = function()
+        for _,mob in pairs(Workspace:GetChildren()) do
+            if mob.Name:find("Boss") and mob:FindFirstChild("Humanoid") then
+                mob.Humanoid.Health = 0
+            end
+        end
+    end
+})
 
--- Visual Tab (unchanged)
--- [Insert your Visual tab here...]
+-- Visual Tab
+local VisualTab = Window:CreateTab("Visual", 4483362458)
+VisualTab:CreateToggle({
+    Name = "Night Vision",
+    CurrentValue = false,
+    Callback = function(val)
+        if val then
+            game.Lighting.Brightness = 10
+        else
+            game.Lighting.Brightness = 2
+        end
+    end
+})
 
 -- Movement Tab
 local MoveTab = Window:CreateTab("Movement", 4483362458)
@@ -95,33 +129,32 @@ RunService.Stepped:Connect(function()
     end
 end)
 
-local flyScript = [[
--- Basic Fly script
-local plr = game.Players.LocalPlayer
-local hum = plr.Character:FindFirstChildOfClass("Humanoid")
-local flying = true
-hum.PlatformStand = true
-local bg = Instance.new("BodyGyro", plr.Character.PrimaryPart)
-local bv = Instance.new("BodyVelocity", plr.Character.PrimaryPart)
-bg.P = 9e4; bg.MaxTorque = Vector3.new(9e9,9e9,9e9)
-bv.Velocity = Vector3.new(0,0,0); bv.MaxForce = Vector3.new(9e9,9e9,9e9)
-game:GetService("UserInputService").InputBegan:Connect(function(inp)
-    if inp.KeyCode == Enum.KeyCode.E then
-        flying = not flying
-        hum.PlatformStand = flying
-        if not flying then bg:Destroy(); bv:Destroy() end
-    end
-end)
-RunService:BindToRenderStep("Fly",200,function()
-    if flying then
-        bv.Velocity = workspace.CurrentCamera.CFrame.LookVector * 50
-        bg.CFrame = workspace.CurrentCamera.CFrame
-    end
-end)
-]]
 MoveTab:CreateButton({
     Name = "Fly (Toggle with E)",
     Callback = function()
+        local flyScript = [[
+        local plr = game.Players.LocalPlayer
+        local hum = plr.Character:FindFirstChildOfClass("Humanoid")
+        local flying = true
+        hum.PlatformStand = true
+        local bg = Instance.new("BodyGyro", plr.Character.PrimaryPart)
+        local bv = Instance.new("BodyVelocity", plr.Character.PrimaryPart)
+        bg.P = 9e4; bg.MaxTorque = Vector3.new(9e9,9e9,9e9)
+        bv.Velocity = Vector3.new(0,0,0); bv.MaxForce = Vector3.new(9e9,9e9,9e9)
+        game:GetService("UserInputService").InputBegan:Connect(function(inp)
+            if inp.KeyCode == Enum.KeyCode.E then
+                flying = not flying
+                hum.PlatformStand = flying
+                if not flying then bg:Destroy(); bv:Destroy() end
+            end
+        end)
+        game:GetService("RunService"):BindToRenderStep("Fly",200,function()
+            if flying then
+                bv.Velocity = workspace.CurrentCamera.CFrame.LookVector * 50
+                bg.CFrame = workspace.CurrentCamera.CFrame
+            end
+        end)
+        ]]
         loadstring(flyScript)()
     end,
 })
